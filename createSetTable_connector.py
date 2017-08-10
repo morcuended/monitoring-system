ef6725c96e1ce612a00ecdb3e34166d86c8b5f96
Daniel Morcuende <dmorcuende@gmail.com>
2017-08-10 17:05:12 +0200

    with simpletable

--- createSetTable_connector.py
+++ createSetTable_connector.py
@@ -1,9 +1,8 @@
 import mysql.connector
 import MySQLdb as mdb
 from mysql.connector import errorcode
-#from warnings import filterwarnings
-
-#filterwarnings('ignore', category = mysql.connector.Warning)
+import numpy as np
+#from __future__ import print_function
 
 # Define database parameters
 myTable="Test_Camera1U_FFCoeff"
@@ -20,7 +19,7 @@ config = {
 
 try:
   cnx = mysql.connector.connect(**config)
-  print("You are connected \n")
+  print("You are now connected \n")
 except mysql.connector.Error as err:
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
     print("Something is wrong with your user name or password \n")
@@ -30,7 +29,7 @@ except mysql.connector.Error as err:
     print(err)
 else:
   cursor=cnx.cursor()
-#################################################
+##########################################3
 cursor.execute("SHOW TABLES;")
 table_list = map(lambda x:x[0],cursor)
 #print table_list
@@ -51,12 +50,14 @@ else:
 		HiCoeff double,
 		HiCoeffRMS double,
 		LoCoeff double,
-		LoCoefdRMS double, 
+		LoCoeffRMS double,
+		Chi2ndfHi double,
+		Chi2ndfLo double, 
 		TelescopeId int,
 		index(SetNum)); """)
-	cnx.commit()
+	print "Table %s just created \n" %myTable
+	cnx.commit() # Make sure data is committed to the database
 
-#####################################################
 if myTable_Set in table_list:
         print "Table %s already exists \n" %myTable_Set
 else:   
@@ -65,6 +66,81 @@ else:
 		TelescopeId int NOT NULL,
 		WhenEntered TIMESTAMP,
 		index(TelescopeId)); """)
-	cnx.commit()
-cnx.close()
-cursor.close()
+	cnx.commit() 
+	print "Table %s just created \n" %myTable_Set
+#cursor.close()
+
+
+
+#cursor.execute("""insert into Test_Camera1U_FFCoeff_Set set SetNum=2,Comments="This is %s table", TelescopeId=1;"""%myTable)
+
+#cursor.execute("insert into SimpleTable set Name=myTable;")
+
+
+#Fill tables
+data=np.genfromtxt("FFcoeff.dat",skip_header=1,comments="#",delimiter=",")
+#print(data[1,])
+
+SetNum = 1
+RunNumber = 128000
+TelescopeId= 1
+
+for row in data:
+	PixId=row[0]
+	HiEntries=row[1]
+	LoEntries=row[2]
+	HiIllumination=row[3]
+	HiIlluminationRMS=row[4]
+	LoIllumination=row[5]
+	LoIlluminationRMS=row[6]
+	HiCoeff=row[7]
+	HiCoeffRMS=row[8]
+	LoCoeff=row[9]
+	LoCoeffRMS=row[10]
+	Chi2ndfHi=row[11]
+	Chi2ndfLo=row[12]
+	cursor.execute("""insert into Test_Camera1U_FFCoeff set 
+			pixel=%i,
+			RunNumber=%i,
+			SetNum=%i,
+			HiIllumination=%f,
+			HiIlluminationRMS=%f,
+			LoIllumination=%f,
+			LoIlluminationRMS=%f,
+			HiCoeff=%f,
+			HiCoeffRMS=%f,
+			LoCoeff=%f,
+			LoCoeffRMS=%f,
+			Chi2ndfHi=%f,
+			Chi2ndfLo=%f,
+			TelescopeId=%i;"""%(PixId,RunNumber,SetNum,HiIllumination,HiIlluminationRMS,LoIllumination,LoIlluminationRMS,
+			HiCoeff,HiCoeffRMS,LoCoeff,LoCoeffRMS,Chi2ndfHi,Chi2ndfLo,TelescopeId))
+
+#Loop over the rows of data and store each value into the corresponding column of the table
+#Check first if a certain SetNum is already in the table 
+cursor.execute("select * from %s"%myTable_Set)
+#print cursor
+#print cursor.fetchall()[0][1]
+SetNum_list = map(lambda x:x[0],cursor)
+#print SetNum_list
+
+if not SetNum in SetNum_list:
+	cursor.execute("""insert into Test_Camera1U_FFCoeff_Set set 
+			SetNum=%i,
+			TelescopeId=%i;"""%(SetNum,TelescopeId))
+
+# Write entry into SimpleTable
+
+cursor.execute("""SELECT COUNT(*) FROM information_schema.columns WHERE table_name="%s";"""%myTable)
+print(cursor.fetchall())     #YOU ARE HEREEEE!!!
+
+#I want to get the number of columns in myTable should be 16-2(entry and SetNum):14
+
+#cursor.execute("""insert into SimpleTable set 
+#		Name="%s",
+#		MaxColumns="%i"
+#		SqlTableSet="%s",
+#		SqlTableData="%s",
+#		SqlSetColumn="SetNum"
+#		SqlSetColumnData="SetNum";"""%(myTable,myTable_Set,myTable))
+
